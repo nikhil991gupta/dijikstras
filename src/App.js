581ddecdf4b77ea146c31b060576feabf4f98ecc
nikhil991gupta <nikhil991gupta@gmail.com>
2023-04-11 23:36:11 +0530

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">   
       <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
