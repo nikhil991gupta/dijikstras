f6daa46ed1fa7ec1207d963922094572a2aee02b
nikhil991gupta <nikhil991gupta@gmail.com>
2023-04-12 23:35:03 +0530

    .

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -8,7 +8,7 @@ const START_NODE_ROW = 10;
 const START_NODE_COL = 15;
 const FINISH_NODE_ROW = 10;
 const FINISH_NODE_COL = 35;
-
+ 
 export default class PathfindingVisualizer extends Component {
   constructor() {
     super();
