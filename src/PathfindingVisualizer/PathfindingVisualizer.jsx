acd15e023f92bf7d2b60bd91f226c299e47a12db
nikhil991gupta <nikhil991gupta@gmail.com>
2023-04-24 23:37:40 +0530

    .

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -112,7 +112,7 @@ export default class PathfindingVisualizer extends Component {
                 })} 
               </div>
             );    
-          })}
+          })} 
         </div>
             
       </> 
